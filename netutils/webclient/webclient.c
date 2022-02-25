4e952f7f6be906c3ba6c7568640a6356d37405bc
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2022-02-25 14:22:28 +0900

    netutils/webclient: Fix bug that the socket is not close
    Fix to close socket when it fails to resolve hostname.

--- netutils/webclient/webclient.c
+++ netutils/webclient/webclient.c
@@ -1063,9 +1063,8 @@ int webclient_perform(FAR struct webclient_context *ctx)
                       /* Could not resolve host (or malformed IP address) */
 
                       nwarn("WARNING: Failed to resolve hostname\n");
-                      free(ws);
-                      _SET_STATE(ctx, WEBCLIENT_CONTEXT_STATE_DONE);
-                      return -EHOSTUNREACH;
+                      ret = -EHOSTUNREACH;
+                      goto errout_with_errno;
                     }
 
                   server_address = (const struct sockaddr *)&server_in;
